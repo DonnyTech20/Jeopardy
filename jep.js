960a837f5aa62e8a252242f1b5f92280aa7b82b5
Donny Techiera <DonnyTech20@gmail.com>
2018-09-17 23:54:52 -0400

    index an js added

--- jep.js
+++ jep.js
@@ -1,17 +1,42 @@
-
-function msg() {
-    alert("Pittsburgh Steelers ");
+function myFunction() {
+    const cat1 = prompt("America Favorite Football Team");
+    if (cat1 != null) {
+        document.getElementById(demo).innerHTML =
+            "Hello " + cat1 + "! How are you today?";
+    }
 };
 
-function country() {
-    alert("USA");
+function myFunction() {
+    const cat2 = prompt("Island has 365 beaches");
+    if (cat2 != null) {
+        document.getElementById(demo2).innerHTML =
+            "Hi " + cat2 + "! Today is a good day";
+    }
+}; 
+
+function myFunction() {
+    var cat3 = prompt("America Favorite Football Team");
+    if (cat3 != null) {
+        document.getElementById("demo3").innerHTML =
+            "Hello " + cat1 + "! How are you today?";
+    }
 };
-var Island = () => {
-    alert ("CUBA");
-}
-var Kenya = () => {
-    alert ("KENYA");
-}
+function myFunction() {
+    var cat4 = prompt("America Favorite Football Team");
+    if (cat4 != null) {
+        document.getElementById("demo4").innerHTML =
+            "Hello " + cat1 + "! How are you today?";
+    }
+}; 
+function myFunction() {
+    var cat5 = prompt("America Favorite Football Team");
+    if (cat5 != null) {
+        document.getElementById("demo5").innerHTML =
+            "Hello " + cat1 + "! How are you today?";
+    }
+} 
+
+
 
 
 
